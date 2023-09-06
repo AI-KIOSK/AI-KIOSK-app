8cdc7122f44b5ff9956919818f98698143a4e29e
yongun2 <djkye0001@naver.com>
2023-09-06 20:06:15 +0900

    [FEAT] 옵션 선택 모달

---
+++ components/menu/MenuOptionItem.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { styled } from 'styled-components/native';
+
+MenuOptionItem.propTypes = {};
+
+export default function MenuOptionItem({ option }) {
+  return <Container></Container>;
+}
+
+const Container = styled.TouchableOpacity``;
---
+++ components/menu/MenuOptionList.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { styled } from 'styled-components/native';
+
+MenuOptionList.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+export default function MenuOptionList({ label, data }) {
+  return (
+    <Container>
+      <Label>{label}</Label>
+      <HorizontalLine />
+      <OptionList />
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  width: 100%;
+  height: 30%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  padding: ${wp(2)}px;
+`;
+
+const Label = styled.Text`
+  color: #002b85;
+  font-size: ${RFValue(18)}px;
+  font-weight: 700;
+`;
+
+const HorizontalLine = styled.View`
+  width: 100%;
+  height: 1px;
+
+  margin-vertical: ${hp(1)}px;
+  background: #002b85;
+`;
+
+const OptionList = styled.ScrollView``;
