c83168d9461056f9349ecd8b46fd640a0155bdfb
kkongchi <smallbean99s@gmail.com>
2023-09-11 17:23:41 +0900

    상단바 필터링 구현

---
+++ components/common/SeniorHeader.jsx
@@ -0,0 +1,129 @@
+import React, { useMemo } from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { useRecoilState } from 'recoil';
+import { Category, Temperature } from 'recoil/Category';
+import styled from 'styled-components';
+
+function SeniorHeader(props) {
+  const menuItems = useMemo(
+    () => [
+      {
+        id: 'coffee',
+        menu: '커피',
+      },
+      {
+        id: 'beverage',
+        menu: '음료',
+      },
+      {
+        id: 'tea',
+        menu: '차',
+      },
+      {
+        id: 'etc',
+        menu: '기타',
+      },
+    ],
+    [],
+  );
+
+  const temperatureItems = useMemo(
+    () => [
+      {
+        id: 'hot',
+        menu: '따뜻한거',
+      },
+      {
+        id: 'ice',
+        menu: '차가운거',
+      },
+    ],
+    [],
+  );
+
+  const [category, setCategory] = useRecoilState(Category);
+  const [temperature, setTemperature] = useRecoilState(Temperature);
+
+  const categoryHandler = (id) => {
+    setCategory(id);
+  };
+  console.log(category);
+
+  const temperatureHandler = (id) => {
+    setTemperature(id);
+  };
+  console.log(temperature);
+
+  return (
+    <Container>
+      <Entypo name="home" size={RFValue(30)} color={'black'} />
+      <CategoryContainer>
+        <TemperatureCategory>
+          {temperatureItems.map((item) => (
+            <TemperatureButton
+              key={item.id}
+              onPress={() => temperatureHandler(item.id)}
+              isSelected={temperature === item.id}
+            >
+              <Label>{item.menu}</Label>
+            </TemperatureButton>
+          ))}
+        </TemperatureCategory>
+        <ItemCategory>
+          {menuItems.map((item) => (
+            <Button key={item.id} onPress={() => categoryHandler(item.id)} isSelected={category === item.id}>
+              <Label>{item.menu}</Label>
+            </Button>
+          ))}
+        </ItemCategory>
+      </CategoryContainer>
+    </Container>
+  );
+}
+
+const Container = styled.SafeAreaView`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: ${hp(16)}px;
+`;
+
+const CategoryContainer = styled.View`
+  flex-direction: column;
+  width: 80%;
+`;
+
+const TemperatureCategory = styled.View`
+  width: 100%;
+  height: 30%;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const ItemCategory = styled(TemperatureCategory)``;
+
+const Button = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ isSelected }) => (isSelected ? '#f29727' : 'transparent')};
+
+  width: ${wp(16)}px;
+  height: ${hp(4)}px;
+  border: 1px;
+  border-radius: 12px;
+`;
+
+const TemperatureButton = styled(Button)`
+  width: ${wp(28)}px;
+`;
+
+const Label = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-weight: bold;
+`;
+
+export default SeniorHeader;
--- components/senior/SeniorMenuList.jsx
+++ components/senior/SeniorMenuList.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { FlatList } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components';
 
@@ -15,32 +14,67 @@ SeniorMenuList.propTypes = {
       price: PropTypes.number,
     }),
   ),
+  onNextPage: PropTypes.func,
+  onPrevPage: PropTypes.func,
 };
 
-function SeniorMenuList({ menuItemsToShow }) {
+function SeniorMenuList({ menuItemsToShow, onNextPage, onPrevPage }) {
+  console.log(menuItemsToShow);
   return (
     <Container>
-      <ListContainer>
-        <FlatList
-          numColumns={1}
-          data={menuItemsToShow}
-          renderItem={({ item }) => <SeniorMenu key={item.id} img={item.img} name={item.name} />}
-        />
-      </ListContainer>
+      <MenuList>
+        {menuItemsToShow.map((item) => (
+          <SeniorMenu key={item.id} img={item.img} name={item.name} />
+        ))}
+      </MenuList>
+
+      <PageButton>
+        <PrevButton onPress={onPrevPage}>
+          <ButtonLabel>이전</ButtonLabel>
+        </PrevButton>
+        <NextButton onPress={onNextPage}>
+          <ButtonLabel>다음</ButtonLabel>
+        </NextButton>
+      </PageButton>
     </Container>
   );
 }
 
 const Container = styled.View`
   width: 100%;
-  height: 75%;
+  flex: 3;
   justify-content: center;
   align-items: center;
   padding: 0px ${RFValue(10)}px;
 `;
 
-const ListContainer = styled.View`
+const MenuList = styled.View`
+  flex: 3;
+  width: 100%;
+`;
+
+const PageButton = styled.View`
+  flex: 0.25;
+  padding: ${RFValue(5)}px 0px ${RFValue(10)}px 0px;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  gap: ${RFValue(32)}px;
+`;
+
+const PrevButton = styled.TouchableOpacity`
+  width: 25%;
   height: 100%;
+  border: 3px solid;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NextButton = styled(PrevButton)``;
+
+const ButtonLabel = styled.Text`
+  font-size: ${RFValue(22)}px;
+  font-weight: bold;
 `;
 
 export default SeniorMenuList;
